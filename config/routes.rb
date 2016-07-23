c8d81c90f4189e22e1fed8046e52bd886a52bcff
Dario Daoud <dadcontact@gmail.com>
2016-07-23 18:30:43 +0000

    add image to article

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  
+  resources :articles
   devise_for :users
-  root to: 'welcome#index'
+  root to: 'articles#index'
 
   #get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
+
 end
