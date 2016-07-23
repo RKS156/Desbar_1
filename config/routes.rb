bfa40c0a0afd036c616bf984101c481a018608f4
Dario Daoud <dadcontact@gmail.com>
2016-07-23 17:11:02 +0000

    version 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+
+  
+  devise_for :users
+  root to: 'welcome#index'
+
+  #get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
